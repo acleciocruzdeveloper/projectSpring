0a6a8b09c56c51a6ee6e9180598e3311df6a5fc2
Aclecio Cruz <aclecioscruz@gmail.com>
2021-11-08 13:23:16 -0300

    Pagination

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,16 +1,14 @@
-import 'bootstrap/dist/css/bootstrap.css';
+import React from 'react'
 
-import 'assets/css/style.css';
+import ReactDOM from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'assets/css/style.css'
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import App from './App';
-
+import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
